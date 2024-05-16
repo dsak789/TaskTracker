fc88e50324291ee9536808693a0908e7a444b15e
dsak789 <dsak.official@gmail.com>
2024-05-16 17:44:31 +0530

    Backend for Vercel Hosting

---
+++ backend/main.py
@@ -0,0 +1,16 @@
+import uvicorn
+from os import getenv
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    port = int(getenv("PORT",8000)) 
+    uvicorn.run("/server:app",host="0.0.0.0",port=port,reload=True) 
--- backend/server.py
+++ backend/server.py
@@ -1,10 +1,8 @@
 from fastapi import FastAPI
-import uvicorn
-from os import getenv
-from mangum import Mangum 
+# from mangum import Mangum 
 from app.routers import tasks,users
 app = FastAPI()
-handler = Mangum(app)
+# handler = Mangum(app)
 
 app.include_router(tasks.taskrouter)
 app.include_router(users.userrouter)
@@ -16,7 +14,3 @@ async def server():
         return{"message":"Hey Great Your API Running... "}
     except:
         return{"message":"Hey Great Your API Running... There Something error"}
-    
-if __name__ == "__main__":
-    port = int(getenv("PORT",8000)) 
-    uvicorn.run("server:app",host="0.0.0.0",port=port,reload=True) 

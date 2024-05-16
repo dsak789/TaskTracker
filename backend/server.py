6eaefc9b23c74775b4b57c568fa38db5fa7093ca
dsak789 <dsak.official@gmail.com>
2024-05-16 17:26:54 +0530

    Backend for Vercel Hosting

--- backend/server.py
+++ backend/server.py
@@ -12,8 +12,10 @@ app.include_router(users.userrouter)
 
 @app.get('/')
 async def server():
-    return{"message":"Hey Great Your API Running... "}
-
+    try:
+        return{"message":"Hey Great Your API Running... "}
+    except:
+        return{"message":"Hey Great Your API Running... There Something error"}
 if __name__ == "__main__":
     port = int(getenv("PORT",8000)) or 8000
     uvicorn.run("server:app",host="0.0.0.0",port=port,reload=True) 

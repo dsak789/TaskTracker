8bed97d90aaf86fb31e65d04b3a2c6219def9b65
dsak789 <dsak.official@gmail.com>
2024-05-16 17:47:12 +0530

    Backend for Vercel Hosting

--- backend/main.py
+++ backend/main.py
@@ -13,4 +13,4 @@ from os import getenv
 
 if __name__ == "__main__":
     port = int(getenv("PORT",8000)) 
-    uvicorn.run("/server:app",host="0.0.0.0",port=port,reload=True) 
+    uvicorn.run("server:app",host="0.0.0.0",port=port,reload=True) 

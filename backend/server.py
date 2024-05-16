9b99228aa325a355fd9703d995b4561ff2bdb040
dsak789 <dsak.official@gmail.com>
2024-05-16 17:58:13 +0530

    Backend for Vercel Hosting

--- backend/server.py
+++ backend/server.py
@@ -1,15 +1,15 @@
 from fastapi import FastAPI
 # from mangum import Mangum 
-# from app.routers import tasks,users
+from app.routers import tasks,users
 app = FastAPI()
 # handler = Mangum(app)
 
-# app.include_router(tasks.taskrouter)
-# app.include_router(users.userrouter)
+app.include_router(tasks.taskrouter)
+app.include_router(users.userrouter)
 
 
 @app.get('/')
-async def server():
+async def APICHECK():
     try:
         return{"message":"Hey Great Your API Running... "}
     except:

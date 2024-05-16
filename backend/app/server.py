1e6e4d33fc3f1cc082493a18232da90276a04434
dsak789 <dsak.official@gmail.com>
2024-05-16 20:52:04 +0530

    Backend for Vercel Hosting

--- backend/app/server.py
+++ backend/app/server.py
@@ -1,9 +1,27 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 # from mangum import Mangum 
 from app.routers import tasks,users
 app = FastAPI()
 # handler = Mangum(app)
 
+
+# origins = [
+#     "http://localhost",
+#     "http://localhost:8080",
+#     "https://example.com",
+#     "https://www.example.com",
+# ]
+
+middleware = CORSMiddleware(
+    allow_origins="*",
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["Authorization", "Content-Type"],
+)
+
+
+app.add_middleware(middleware)
 app.include_router(tasks.taskrouter)
 app.include_router(users.userrouter)
 

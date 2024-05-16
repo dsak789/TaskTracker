6efd228ea30c568076ef2c666cc4c120bcc09cc7
dsak789 <dsak.official@gmail.com>
2024-05-16 20:57:00 +0530

    Backend for Vercel Hosting

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,11 @@
 import uvicorn
 from os import getenv
 
+print("Welcome TO TASKTRACKER")
 
 if __name__ == "__main__":
-    port = int(getenv("PORT",8000)) 
-    uvicorn.run("app.server:app",host="0.0.0.0",port=port,reload=True) 
+    try:
+        port = int(getenv("PORT",8000)) 
+        uvicorn.run("app.server:app",host="0.0.0.0",port=port,reload=True)
+    except():
+        print("Something Error")

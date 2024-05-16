ec72efb91c35fa3f70f42c17fc502f15d762b076
dsak789 <dsak.official@gmail.com>
2024-05-16 15:52:24 +0530

    Mongodb ATlas is COnnected and so Feature are added

--- backend/server.py
+++ backend/server.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
+from mangum import Mangum
 from app import dbconfig as dbclient
 # from bson import ObjectId
 from app.routers import tasks,users
 app = FastAPI()
-
+handler = Mangum(app)
 # db = dbclient.client["tasks"]
 
 # res = db.insert_many([{"task":"someting5","description":"anything5"},{"task":"somthing6","description":"anything6"}])

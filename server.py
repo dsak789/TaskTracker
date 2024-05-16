84749ff9223c14861259d0f79ec95d232e861263
dsak789 <dsak.official@gmail.com>
2024-05-16 16:19:54 +0530

    Backend for AWS Lambda Function

--- server.py
+++ server.py
@@ -1,15 +1,8 @@
 from fastapi import FastAPI
 from mangum import Mangum
-from app import dbconfig as dbclient
-# from bson import ObjectId
 from app.routers import tasks,users
 app = FastAPI()
 handler = Mangum(app)
-# db = dbclient.client["tasks"]
-
-# res = db.insert_many([{"task":"someting5","description":"anything5"},{"task":"somthing6","description":"anything6"}])
-# print("Data Inserted",res)
-# print("#Records Inserted",res.inserted_ids)
 
 app.include_router(tasks.taskrouter)
 app.include_router(users.userrouter)

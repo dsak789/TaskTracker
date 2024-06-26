bce464dc7d875edff56500d3dfd7446ba5d87c50
dsak789 <dsak.official@gmail.com>
2024-06-26 13:20:40 +0530

    NODEv2

--- backendNode/db/dbConfig.js
+++ backendNode/db/dbConfig.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
 
-mongoUrl = process.env.MONGO_URL
+mongoUrl = process.env.MONGO_URL || "mongodb+srv://sbcreations:sbcreations@cluster0.ao6t0sl.mongodb.net/tasktracker?"
 const conDB =async () =>{       
     await mongoose.connect(mongoUrl)
     .then(()=>{

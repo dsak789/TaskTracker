6c051087177efa0aea61b2f65e182d6f43f77763
dsak789 <dsak.official@gmail.com>
2024-06-26 15:36:37 +0530

    NODEv2

--- backendNodev2/routes/Tasksouter.js
+++ backendNodev2/routes/Tasksouter.js
@@ -6,6 +6,8 @@ const tasks = require('../apis/TasksApi')
 router.post('/addtask',tasks.addTask)
 router.get('/tasks',tasks.getTasks)
 router.get('/tasks/:userid',tasks.getUserTasks)
+router.get('/completed-tasks/:userid',tasks.completedTasks)
+router.get('/archieved-tasks/:userid',tasks.archievedTasks)
 router.get('/updatetask/:taskid/:taskStatus',tasks.updateTask)
 router.get('/deletetask/:taskid',tasks.deleteTask)
 

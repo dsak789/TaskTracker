672347a43a3203e5c8d54e26c97062763e360a36
dsak789 <dsak.official@gmail.com>
2024-06-26 15:25:17 +0530

    NODEv2

--- backendNodev2/routes/Tasksouter.js
+++ backendNodev2/routes/Tasksouter.js
@@ -3,10 +3,9 @@ const router = express.Router()
 
 const tasks = require('../apis/TasksApi')
 
-
-
+router.post('/addtask',tasks.addTask)
 router.get('/tasks',tasks.getTasks)
 
-
+router.get('/deletetask/:taskid',tasks.deleteTask)
 
 module.exports = router

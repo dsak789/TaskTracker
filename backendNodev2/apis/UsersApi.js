9d4a77f8910f6cfd55591bffb6e9c1a1ca67b386
dsak789 <dsak.official@gmail.com>
2024-09-25 00:23:35 +0530

    Added Signup mail when registered

--- backendNodev2/apis/UsersApi.js
+++ backendNodev2/apis/UsersApi.js
@@ -1,6 +1,6 @@
 const User = require('../models/UsersSchema')
 const bcrypt = require('bcrypt')
-
+const reg_mail = require('./RegisterMail')
 
 const verifypwd = async (pwd,encpwd) =>{
     try {
@@ -30,12 +30,14 @@ exports.login = async (req,res)=>{
         const user = await User.findOne({'username':`${username}`})
         if (user.username) {
             if (await verifypwd(password,user.password)){
+                const gitdata = await (await fetch(`https://api.github.com/users/${user.githubid}`)).json()
                 res.json({
                     message:"Login Successfull",
                     user:{
                         "name":user.name,
                         'username':user.username,
-                        'githubid':user.githubid
+                        'githubid':user.githubid,
+                        "dp_url":gitdata.avatar_url || "https://static.vecteezy.com/system/resources/previews/000/649/115/original/user-icon-symbol-sign-vector.jpg"
                     }
                 })
             }
@@ -61,8 +63,23 @@ exports.adduser = async (req,res)=>{
     try {
         const {name,email,githubid,username,password} = req.body
         pwd = await encyptpwd(password)
+        const find = await User.findOne({'username':username})
+        
+        if(!name || !email || !username || !password){
+            res.json({
+                message:"Please give required info",
+            })
+            return
+        }
+        if(find){
+            res.json({
+                message:"User Name already Exists Use Another Username",
+            })
+            return
+        }
         const insert = new User({name,email,githubid,username,'password':pwd})
         await insert.save();
+        await reg_mail.send_signup_mail(username)
         res.json({
             message:"User Registration Successfull..",
             user_details:{name,email,githubid,username,'password':pwd}
@@ -79,7 +96,8 @@ exports.getusers = async (req,res)=>{
         const users = await User.find()
         res.json({
             message:"Users Data Retreived Successfully",
-            users
+            users,
+            "users_count":users.length
         })
     } catch (error) {
         res.status(400).json({meaasage:`${error}`})

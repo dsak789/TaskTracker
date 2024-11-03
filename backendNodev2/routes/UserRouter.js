f63031174ae5612566d51222d0f1dd095fc9538b
dsak789 <dsak.official@gmail.com>
2024-11-03 21:21:28 +0530

    Update Mailer Set

--- backendNodev2/routes/UserRouter.js
+++ backendNodev2/routes/UserRouter.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const users = require('../apis/UsersApi')
 const password = require('../apis/PasswordReset')
-
+const updates = require('../apis/NewUpdateMails')
 
 router.post('/adduser',users.adduser)
 router.post('/login',users.login)
@@ -10,4 +10,5 @@ router.get('/allusers',users.getusers)
 router.get('/deleteuser/:id',users.deleteUser)
 router.post('/forgotpassword/',password.reset_password)
 router.post('/changepassword',password.changePassword)
+router.get('/postupdate',updates.send_new_update_info)
 module.exports=router
